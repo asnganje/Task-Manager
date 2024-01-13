ca9cb3ca8c44fdb3e490a1c0ce68830b35e640ee
asnganje <abd.nganje@gmail.com>
2024-01-13 10:09:20 +0300

    Add connection to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = require('./routes')
+const connectDB = require('./db/connect')
 const app = express()
 require('dotenv').config()
 
@@ -7,7 +8,15 @@ app.use(express.json())
 app.use('/api/v1/tasks', router)
 
 const port = process.env.PORT;
-
-app.listen(port, ()=> {
-    console.log(`Server is listening on port ${port}...`);
-})
+const url = process.env.MONGO_URI;
+const start = async()=> {
+    try {
+        await connectDB(url)
+        app.listen(port, ()=> {
+            console.log(`Server is listening on port ${port}...`);
+        })
+    } catch (error) {
+        console.log('DB connection was unsuccessful');
+    }
+}
+start()
