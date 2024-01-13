f439e5ff4fdd1fbe0810f425e6c71fc07f18b25b
asnganje <abd.nganje@gmail.com>
2024-01-13 10:37:01 +0300

    Pass store state to front end using Provider

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,9 +1,12 @@
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { Provider } from 'react-redux'
+import store from './store/index.js'
 const el = document.getElementById('root')
 const root = createRoot(el)
 root.render(
-  <App />
+  <Provider store={store}>
+      <App />
+  </Provider>
 )
