f8c87323b48b667acda1ad786427d72a9d050be2
asnganje <abd.nganje@gmail.com>
2024-01-13 11:09:16 +0300

    fix schema details

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose')
 
 const taskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'please provide task name'],
+        trim: true,
+        maxLength: [20, 'A maximum of 20 characters allowed']
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 
 })
 
