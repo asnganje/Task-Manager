9d8cc69e60fca5b7af1ad703c97dad59d82840ba
asnganje <abd.nganje@gmail.com>
2024-01-13 09:37:09 +0300

    Added checkbox and delete component in taskItem

--- frontEnd/src/components/TaskItem.jsx
+++ frontEnd/src/components/TaskItem.jsx
@@ -1,7 +1,13 @@
+import { MdDelete } from "react-icons/md";
+
 const TaskItem = () => {
     return(
-        <div className="shadow-2xl p-3 w-[60vh]">
-            Task Item
+        <div className="flex justify-between shadow-2xl p-3 w-[60vh]">
+            <p>Task Item</p>
+            <div className="flex">
+                <input type="checkbox" className="p-1"/>
+                <MdDelete className="text-2xl"/>
+            </div>
         </div>
     )
 }
