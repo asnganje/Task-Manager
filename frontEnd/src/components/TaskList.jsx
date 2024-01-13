d3ed98c4293bfbb1d45f1c5177c091e5eec83fa3
asnganje <abd.nganje@gmail.com>
2024-01-13 20:05:43 +0300

    fix useeffect bug

--- frontEnd/src/components/TaskList.jsx
+++ frontEnd/src/components/TaskList.jsx
@@ -1,10 +1,25 @@
 import TaskItem from "./TaskItem"
+import { useSelector} from 'react-redux'
 
 const TaskList = () => {
 
+    const {data, isLoading} = useSelector((store)=>store.tasks)
+    if(isLoading) {
+        return(
+            <h3>Loading...</h3>
+        )
+    }
+
+    const renderedTasks = data.map((task)=> {
+        return(
+            <div key={task._id}>
+                 <TaskItem task = {task} />   
+            </div>
+        )
+    })
     return(
         <div>
-            <TaskItem />
+            {renderedTasks}
         </div>
     )
 }
--- frontEnd/src/components/dashBoard.jsx
+++ frontEnd/src/components/dashBoard.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react"
 import TaskCreate from './TaskCreate'
 import TaskList from './TaskList'
+import { getTasks } from "../store/thunks/tasksThunk"
+import { useDispatch } from "react-redux"
 
 const DashBoard = () => {
+    const dispatch = useDispatch()
+
+    useEffect(()=> {
+        dispatch(getTasks())
+    }, [])
+    
     return(
         <div  className="flex flex-col items-center p-5 font-mono">
             <TaskCreate />
